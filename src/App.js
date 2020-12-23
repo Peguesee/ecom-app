37a135bd9986fd9397684c9a469ed8acd733c748
Glenn Peguese <peguesee@gmail.com>
2020-12-22 23:52:57 -0800

    Update Navigation add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { HomePage } from './components/pages/homepage.component.jsx';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
+
+const HatsPage = () => (
+    <div>
+        <h1>Hats Page</h1>
+    </div>
+);
 
 function App() {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/shop/:item" component={HatsPage} />
+            </Switch>
         </div>
     );
 }
